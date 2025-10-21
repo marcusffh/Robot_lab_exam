1aba9e863ffa8903cbcf2b57ddced0d2d1d9087c
monniiaa <sofielarsen2411@gmail.com>
2025-10-21 13:11:17 +0200

    s

---
+++ self-localization/test.py
@@ -0,0 +1,40 @@
+# test_move_forward.py
+from explore_landmarks import LocalizationPathing
+from Utils.CalibratedRobot import CalibratedRobot
+import camera
+import numpy as np
+import time
+
+# Initialize robot and camera
+robot = CalibratedRobot()
+cam = camera.Camera()  # or however you initialize your camera
+
+# Dummy landmarks list (won't be used in this test)
+required_landmarks = []
+
+# Initialize LocalizationPathing
+localization = LocalizationPathing(robot, cam, required_landmarks)
+
+# Create a dummy estimated pose object
+class DummyPose:
+    def __init__(self, x=0, y=0, theta=0):
+        self._x = x
+        self._y = y
+        self._theta = theta
+    def getX(self):
+        return self._x
+    def getY(self):
+        return self._y
+    def getTheta(self):
+        return self._theta
+
+# Current pose at origin facing along x-axis
+est_pose = DummyPose(0, 0, 0)
+
+# Goal is 100 cm ahead along x-axis
+goal = np.array([100, 0])
+
+# Move towards the goal
+distance, angle = localization.move_towards_goal_step(est_pose, goal)
+
+print(f"Moved {distance} cm and turned {np.degrees(angle)} degrees")
