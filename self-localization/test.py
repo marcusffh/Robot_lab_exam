f3e4e30c4d8c4ed72bf39a5288d001e1ea3aeb5b
monniiaa <sofielarsen2411@gmail.com>
2025-10-21 13:25:06 +0200

    s

--- self-localization/test.py
+++ self-localization/test.py
@@ -1,3 +1,10 @@
+# test_move_forward.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import numpy as np
+import time
+
 # test_rotate.py
 from Utils.CalibratedRobot import CalibratedRobot
 import time
@@ -6,7 +13,7 @@ import time
 robot = CalibratedRobot()
 
 # Rotate 45 degrees
-angle_deg = 90
+angle_deg = 180
 robot.turn_angle(angle_deg)
 
 print(f"Robot rotated {angle_deg} degrees")
