4309ef3cee8b74c949b7c2af9c5dd76bbad07bd0
monniiaa <sofielarsen2411@gmail.com>
2025-10-21 13:15:13 +0200

    s

--- self-localization/LocalizationPathing.py
+++ self-localization/LocalizationPathing.py
@@ -7,22 +7,44 @@ import numpy as np
 import time
 
 class LocalizationPathing:
-    def __init__(self, robot, camera, required_landmarks, step_cm=20, rotation_deg=20, min_landmarks_to_see = 2):
+    def __init__(self, robot, step_cm=20, rotation_deg=20, min_landmarks_to_see = 2):
         self.robot = robot
-        self.camera = camera
-        self.required_landmarks = set(required_landmarks)
-        self.step_cm = step_cm
-        self.rotation_deg = rotation_deg
-        self.min_landmarks_to_see = min_landmarks_to_see
+        #self.camera = camera
+        #self.required_landmarks = set(required_landmarks)
+        #self.step_cm = step_cm
+        #self.rotation_deg = rotation_deg
+        #self.min_landmarks_to_see = min_landmarks_to_see
 
-        self.observed_landmarks = set()
-        self.min_landmarks_met = False
+        #self.observed_landmarks = set()
+        #self.min_landmarks_met = False
 
-    def explore_step(self, drive=False, min_dist = 400):
-        dist = 0
-        angle_deg = self.rotation_deg 
-        angle_rad = np.radians(angle_deg)
+   # def explore_step(self, drive=False, min_dist = 400):
+   #     dist = 0
+   #     angle_deg = self.rotation_deg 
+   #     angle_rad = np.radians(angle_deg)
+    def move_towards_goal_step(self, est_pose, goal):
+        robot_pos = np.array([est_pose.getX(), est_pose.getY()])
+        direction = goal - robot_pos
+        distance_to_goal = np.linalg.norm(direction)
+        angle_to_goal = np.arctan2(direction[1], direction[0]) - est_pose.getTheta()
+        
+        move_dist = distance_to_goal
+        angle_to_goal = (angle_to_goal + np.pi) % (2 * np.pi) - np.pi
+        
+        print(f"distance moved: {distance_to_goal}")
+        print(f"angle (rad) turned: {angle_to_goal}")
 
+        self.robot.turn_angle(np.degrees(angle_to_goal))
+
+        self.robot.drive_distance_cm(move_dist)
+        self.robot.stop()
+        time.sleep(0.2)
+
+        return distance_to_goal, angle_to_goal
+
+
+
+"""
         if self.min_landmarks_met:
             return 0, 0 
 
@@ -58,31 +80,7 @@ class LocalizationPathing:
 
 
     def seen_enough_landmarks(self):
-        """
-        Returns True if at least `min_landmarks_seen` have been observed.
-        """
         return self.min_landmarks_met
-    
+    """
 
     
-    def move_towards_goal_step(self, est_pose, goal):
-        robot_pos = np.array([est_pose.getX(), est_pose.getY()])
-        direction = goal - robot_pos
-        distance_to_goal = np.linalg.norm(direction)
-        angle_to_goal = np.arctan2(direction[1], direction[0]) - est_pose.getTheta()
-        
-        move_dist = distance_to_goal
-        angle_to_goal = (angle_to_goal + np.pi) % (2 * np.pi) - np.pi
-        
-        print(f"distance moved: {distance_to_goal}")
-        print(f"angle (rad) turned: {angle_to_goal}")
-
-        self.robot.turn_angle(np.degrees(angle_to_goal))
-
-        self.robot.drive_distance_cm(move_dist)
-        self.robot.stop()
-        time.sleep(0.2)
-
-        return distance_to_goal, angle_to_goal
-
-
