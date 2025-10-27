febe57fde0ea56013eaf598cc40fc27c106e5ec7
monniiaa <sofielarsen2411@gmail.com>
2025-10-27 16:53:01 +0100

    s

--- localization/LocalizationPathing.py
+++ localization/LocalizationPathing.py
@@ -53,6 +53,7 @@ class LocalizationPathing:
             self.observed_landmarks.add(landmarkID)
 
             if len(self.observed_landmarks) >= self.min_landmarks_to_see:
+                print("seen enough")
                 self.min_landmarks_met = True
 
     def seen_enough_landmarks(self):
