644f47b4fe9d8f9e0f079ce7f930de11af73b790
monniiaa <sofielarsen2411@gmail.com>
2025-10-27 13:11:39 +0100

    s

--- localization/particle.py
+++ localization/particle.py
@@ -155,8 +155,8 @@ def resample_particles(particle_list):
     total_weight = np.sum(weights)
     weights /= total_weight
 
-    particle_list = particle_list[weights > 0]
-    weights = weights[weights > 0]
+    particle_list = [p for p, w in zip(particle_list, weights) if w > 0]
+    weights = np.array([w for w in weights if w > 0])
 
     cdf = np.cumsum(weights)
     resampled = []
